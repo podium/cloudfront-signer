2bde21c57d6ea08097eb6ec492f4949453b93c3b
John McGuin <john@waysoftware.dev>
2024-01-15 15:47:50 -0700

    task: update to current rc elixir

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,11 @@
-use Mix.Config
+import Config
 
 config :cloudfront_signer, CloudfrontSignerTest,
   domain: "https://somewhere.cloudfront.com",
   key_pair_id: "a_key_pair",
-  private_key: """
-"""
+  private_key:
+    System.get_env("PRIVATE_KEY") ||
+      raise("""
+      environment variable PRIVATE_KEY is missing.
+      You can generate one by calling: openssl genpkey -algorithm RSA
+      """)
--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule CloudfrontSigner.MixProject do
   def project do
     [
       app: :cloudfront_signer,
-      version: "0.1.0",
-      elixir: "~> 1.6",
+      version: "0.2.0",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -13,15 +13,15 @@ defmodule CloudfrontSigner.MixProject do
 
   def application do
     [
-      extra_applications: [:logger],
-      mod: {CloudfrontSigner.Application, []}
+      mod: {CloudfrontSigner.Application, []},
+      extra_applications: [:logger]
     ]
   end
 
   defp deps do
     [
-      {:poison, "~> 3.1"},
-      {:timex, "~> 3.1"}
+      {:jason, "~> 1.4"},
+      {:timex, "~> 3.7"}
     ]
   end
 end
