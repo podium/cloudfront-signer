c1a5c5d3e62c3da212de22bfc6e3d12d70952fac
Clifton McIntosh <clifton.mcintosh@podium.com>
2025-02-08 09:41:54 -0600

    Set key for running tests

--- config/test.exs
+++ config/test.exs
@@ -4,8 +4,8 @@ config :cloudfront_signer, CloudfrontSignerTest,
   domain: "https://somewhere.cloudfront.com",
   key_pair_id: "a_key_pair",
   private_key:
-    System.get_env("PRIVATE_KEY") ||
+    System.get_env("TESTING_PRIVATE_KEY") ||
       raise("""
-      environment variable PRIVATE_KEY is missing.
+      environment variable TESTING_PRIVATE_KEY is missing.
       You can generate one by calling: openssl genpkey -algorithm RSA
       """)
