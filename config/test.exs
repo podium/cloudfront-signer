58fa77fdc871e910368664755fda1b3b95e2f06c
Clifton McIntosh <clifton.mcintosh@podium.com>
2025-02-08 10:18:41 -0600

    Add tests for DistributionRegistry and Distribution

--- config/test.exs
+++ config/test.exs
@@ -1,11 +1,23 @@
 import Config
 
 config :cloudfront_signer, CloudfrontSignerTest,
-  domain: "https://somewhere.cloudfront.com",
-  key_pair_id: "a_key_pair",
+  domain: "https://test.cloudfront.net",
   private_key:
     System.get_env("TESTING_PRIVATE_KEY") ||
       raise("""
       environment variable TESTING_PRIVATE_KEY is missing.
-      You can generate one by calling: openssl genpkey -algorithm RSA
-      """)
+      You can use the test/support/test_private_key.pem file for your tests.
+      See the .env.sample file for more information.
+      You can also generate one by calling: openssl genpkey -algorithm RSA
+      """),
+  key_pair_id: "a_key_pair"
+
+config :cloudfront_signer,
+  domain: "https://test.cloudfront.net",
+  private_key: System.get_env("TESTING_PRIVATE_KEY"),
+  key_pair_id: "a_key_pair"
+
+config :cloudfront_signer, CloudfrontSigner.DistributionRegistryTest,
+  domain: "https://test.cloudfront.net",
+  private_key: System.get_env("TESTING_PRIVATE_KEY"),
+  key_pair_id: "a_key_pair"
